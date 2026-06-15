fc88124f1a053e166bb00dda5db1ff1f4b8c7ed2
louisrid <louisjridland@gmail.com>
2026-06-15 13:30:42 +0100

    Add files via upload

--- server/game.js
+++ server/game.js
@@ -1676,7 +1676,7 @@ class Game {
           squad: me.squad.map((p) => ({ name: p.name, pos: p.pos, injured: p.name === me.injured, rtg: p.rating, wonderkid: !!p.wonderkid, grew: p.grew || 0 })),
         };
       })() : null,
-      serverV: 'v12.4',
+      serverV: 'v12.5',
       paused: this.paused,
       hostPaused: !!this.hostPaused,
     };
