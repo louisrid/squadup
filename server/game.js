8395538120736fd5ee49b0c3494203b480f0634b
louisrid <louisjridland@gmail.com>
2026-06-17 02:30:54 +0100

    Add files via upload

--- server/game.js
+++ server/game.js
@@ -1906,7 +1906,7 @@ class Game {
           })),
         };
       })() : null,
-      serverV: 'v18.7',
+      serverV: 'v18.8',
       paused: this.paused,
       hostPaused: !!this.hostPaused,
     };
